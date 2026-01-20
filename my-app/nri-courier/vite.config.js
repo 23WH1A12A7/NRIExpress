5474c34701824ad435e72afd6bc09f681d9f2caa
shivanialugoju <shivanialugoju@gmail.com>
2026-01-20 22:23:39 +0530

    Vite build config added

--- my-app/nri-courier/vite.config.js
+++ my-app/nri-courier/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/NRIExpress/',   // 👈 VERY IMPORTANT
   plugins: [react()],
+  base: '/NRIExpress/',   // 👈 repository name
 })
